b263366c0913b73cf81475d180be9e0f47f9450d
Rahul Duggal <duggalr42@gmail.com>
2023-08-29 09:11:52 -0400

    changes

--- learning_assistant/main_utils.py
+++ learning_assistant/main_utils.py
@@ -20,7 +20,7 @@ def get_embedding(text, model="text-embedding-ada-002"):
 
 def main_handle_question(question, student_code):
     
-    q_prompt = """#Instructions:
+    q_prompt = """##Instructions:
 You are a Python Programming Tutor / Teacher.
 Your goal is to help the student achieve their objective, through the problem they present you.
 Some potential problems you may get asked include getting help debugging code below, getting further information or examples on a concept or python library, or the student may ask you to generate questions to help them better understand a concept.
@@ -31,6 +31,7 @@ If you sense the student is discouraged, frustrated, or is just simply looking f
 The students are new to programming, and your role in helping them become better programmers will be critical. 
 If you need further clarification, please ask the student.
 You will be provided with the student's question, along with their code (although note: the code may not always be relevant as sometimes they might be asking to learn about a concept more generally).
+Again, provide the students with hints and meaningful information on how they can achieve the right answer, but do not give them the answer.
 
 ##Student Question:
 {question}
@@ -40,7 +41,6 @@ You will be provided with the student's question, along with their code (althoug
 
 ##Your Answer:
 """
-
     # q_embd = list(get_embedding(question))
     question = question.strip()
     student_code = student_code.strip()
@@ -54,8 +54,8 @@ You will be provided with the student's question, along with their code (althoug
     di = {"role": "user", "content": q_prompt}
     messages_list = [di]
     response = openai.ChatCompletion.create(
-        # model = "gpt-4",
-        model = "gpt-3.5-turbo-16k-0613",
+        model = "gpt-4",
+        # model = "gpt-3.5-turbo-16k-0613",
         messages = messages_list,
     )
     response_message = response["choices"][0]["message"]['content']

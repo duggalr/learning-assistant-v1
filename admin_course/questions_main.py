f9a3a1c40f151c67aa4a66d1382b27e9a77adc80
Rahul Duggal <duggalr42@gmail.com>
2023-09-07 09:18:21 -0400

    upadting question

--- admin_course/questions_main.py
+++ admin_course/questions_main.py
@@ -21,7 +21,7 @@ from learning_assistant.models import *
 q_files_list = [
     # ['lesson_1_desc.txt', 'lesson_1_questions.txt'],
     # ['lesson_2_desc.txt', 'lesson_2_questions.txt'],
-    # ['lesson_3_desc.txt', 'lesson_3_questions.txt']
+    # ['lesson_3_desc.txt', 'lesson_3_questions.txt'],
     # ['lesson_4_desc.txt', 'lesson_4_questions.txt'],
     # ['lesson_5_desc.txt', 'lesson_5_questions.txt'],
 ]
@@ -30,32 +30,34 @@ for li in q_files_list:
     lesson_desc_fp, lesson_questions_fp = li[0], li[1]
 
     lesson_desc = open(lesson_desc_fp).read()
-    lesson_questions = open(lesson_questions_fp).readlines()
+    # lesson_questions = open(lesson_questions_fp).readlines()
 
     lesson_desc_list = lesson_desc.split('Description:')
     lesson_name, lesson_desc_text = lesson_desc_list[0].strip(), lesson_desc_list[1].strip()
 
     print(lesson_name, lesson_desc_text)
 
-    # l_obj = Lesson.objects.create(
-    #     title = lesson_name,
-    #     description = lesson_desc_text
-    # )
-    # l_obj.save()
+    l_obj = Lesson.objects.create(
+        title = lesson_name,
+        description = lesson_desc_text
+    )
+    l_obj.save()
 
-    # for lq in lesson_questions:
-    #     full_sentence = lq.strip()
-    #     tmp_li = full_sentence.split(': ')
-    #     print(tmp_li)
-    #     question_name = tmp_li[0].strip()
-    #     question_text = tmp_li[1].strip()
+    with open(lesson_questions_fp, "r") as f:
+        content = f.read()
+        lesson_q = content.split('\n\n')
 
-    #     lq_obj = LessonQuestion.objects.create(
-    #         question_name = question_name,
-    #         question_text = question_text,
+        for lq in lesson_q:
+            full_sentence = lq.strip()
+            # print(full_sentence)
+ 
+            question_name = full_sentence.split('\n')[0].split(': ')[-1].strip()
+            print(question_name, full_sentence)
 
-    #         lesson_obj = l_obj
-    #     )
-    #     lq_obj.save()
-
+            lq_obj = LessonQuestion.objects.create(
+                question_name = question_name,
+                question_text = full_sentence,
+                lesson_obj = l_obj
+            )
+            lq_obj.save()
 

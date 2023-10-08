e3ba75c1dc64f45fca1112ca76c73aaec82bb94e
Rahul Duggal <duggalr42@gmail.com>
2023-10-08 16:05:46 -0400

    prompt update

--- learning_assistant/main_utils.py
+++ learning_assistant/main_utils.py
@@ -299,8 +299,9 @@ for idx in list_one:
 ##Example Good Answer: (this is a good answer because it identifies the mistake the student is making but instead of correcting it for the student, it asks the student a follow-up question as a hint, forcing the student to think on their own)
 You are on the right track. Pay close attention to the operation you are performing in the loop. You're currently multiplying the number with itself, but you want to find the product of all numbers. What operation should you use instead to continuously update 'total_product'?
 
-Based on the conversation, try to always ask follow-up questions to the individual. 
+Based on the conversation, try to always ask meaningful follow-up questions to the individual. 
 This is a great way to foster a more engaging conversation, and help the individual gain a more deeper understanding of the material they are trying to learn.
+However, if you feel the student has received the information they need and there is no meaningful follow-up question you can think of, please close out the conversation by thanking the student and telling them they can ask any other questions if they wish.
 
 ##Previous Chat History with Student:
 {previous_chat_history_st}

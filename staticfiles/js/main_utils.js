69ee4449b7ad9ae5f48128880b960f5416e03177
duggalr <duggalr42@gmail.com>
2024-04-01 21:08:56 -0400

    req

---
+++ staticfiles/js/main_utils.js
@@ -0,0 +1,19 @@
+function generateUniqueUserID(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        counter += 1;
+    }
+    return result;
+};
+
+
+// $('#signup-login-button').click(function(){
+
+//     let new_url = window.location.protocol + '//' + window.location.hostname + ':' + window.location.port + '/login';
+//     window.location.replace(new_url);
+
+// });

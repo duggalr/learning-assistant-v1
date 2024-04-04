458b07e83ff9e4dfb5d0a9089ce17eba98fff3c5
lambda-rahul <124808899+lambda-rahul@users.noreply.github.com>
2024-04-04 10:13:06 -0400

    experimentation

--- experimentation/course_gen/1_initial_student_description_generation.py
+++ experimentation/course_gen/1_initial_student_description_generation.py
@@ -2,16 +2,6 @@ import os
 from dotenv import load_dotenv, find_dotenv
 from openai import OpenAI
 
-
-if 'PRODUCTION' not in os.environ:
-    dot_env_file = find_dotenv()
-    load_dotenv(dot_env_file)
-
-API_KEY = os.environ['OPENAI_API_KEY']
-client = OpenAI(
-    api_key = API_KEY,
-)
-
 # objective/goal
 # breadth of background material for AI will be critical
 
@@ -43,9 +33,20 @@ client = OpenAI(
             # High-level concepts/tags that were covereed 
 
 
+
+if 'PRODUCTION' not in os.environ:
+    dot_env_file = find_dotenv()
+    load_dotenv(dot_env_file)
+
+API_KEY = os.environ['OPENAI_API_KEY']
+client = OpenAI(
+    api_key = API_KEY,
+)
+
 previouse_student_chat_history = ""
 c = 1
-output_dir_fp = '/Users/rahulduggal/Documents/personal_learnings/learning-assistant-v1/experimentation/course_gen/gpt_4_output'
+output_dir_fp = '/Users/rahulduggal/Documents/personal_learnings/learning-assistant-v1/experimentation/course_gen/course_generation_round_one'
+
 while True:
 
     # Your overall goal is to generate a starting course module for a student.
@@ -53,7 +54,6 @@ while True:
 
     q_prompt = """##Instructions:
 A student will approach you wanting to learn about a specific field/concept.
-Your goal is to determine what the student wants to learn and their background.
 Your goal is to precisely learn what the student wants to learn, their motivations, and their background.
 You goal is NOT to generate a course outline/syllabus. It is simply to learn about the student and what they want to learn.
 You will primarily do this through having a natural, genuine conversation with the student.
@@ -64,7 +64,7 @@ ONLY ASK ONE QUESTION AT A TIME.
 Here are 4 high-level guiding questions on what you can probe the student on:
 1. What does the student want to learn?
     - You need to have a deep understanding here on precisely what the student is trying to learn.
-    - Example: If student says, "symmetric key cryptography to encrypt/decrypt", what do they exactly mean by that?
+    - Example: If the student says, "symmetric key cryptography to encrypt/decrypt", what do they exactly mean by that?
         - do they want to learn stream ciphers, one-time pads, block ciphers, or all of it?
         - do they want a high level understanding of how the algorithms work so they can build something?
         - do they want a low level understanding of the algorithms and why they are secure?
@@ -100,7 +100,7 @@ Below, you are given the current student response, along with any previous conve
 
 ##Your Answer:
 """
-    
+
     user_response = input("Question: ")
     user_response = user_response.strip()
 
@@ -110,8 +110,7 @@ Below, you are given the current student response, along with any previous conve
     )
     # print(q_prompt)
 
-    tmp_fp = os.path.join(output_dir_fp, f"Q-Prompt-Round-Four-{c}.txt")
-    # f = open(f"Q-Prompt-{c}.txt", 'w')
+    tmp_fp = os.path.join(output_dir_fp, f"Student-Background-Description-Count-{c}.txt")
     f = open(tmp_fp, 'w')
     f.write(q_prompt)
     f.close()
--- experimentation/course_gen/main_three.py
+++ experimentation/course_gen/main_three.py
@@ -3,11 +3,6 @@ from dotenv import load_dotenv, find_dotenv
 from openai import OpenAI
 
 
-# Week 1: Introduction to Cryptography and Symmetric Cryptography
-# - Day 1-2: Basics of cryptography and its applications
-# - Day 3-4: Overview of symmetric cryptography
-# - Day 5: Practical examples and uses of symmetric cryptography"
-
 if 'PRODUCTION' not in os.environ:
     dot_env_file = find_dotenv()
     load_dotenv(dot_env_file)
@@ -18,17 +13,15 @@ client = OpenAI(
 )
 
 q_prompt = """##Instructions:
-Your goal is to generate course notes which the student will use, to help them understand the material for the specific week.
-The goal here is to generate course notes, specific for the student, given their goals and background.
-The goal is for you to generate the course notes in such a manner that make it very easy for the student to understand the material.
+Your goal is to generate course notes on a specific topic that the student will use, to help them develop a strong understanding of that topic.
+Your course notes must be generated such that it is specific for the student, given their goals and background.
+The notes should be generated such that, it is very engaging and easy for the student to understand the material.
 Please ensure your course notes for the week are as DETAILED AS POSSIBLE. The more detail, the better for the student.
-Please understand that your course notes will be the primary resource for the student, as they are learning the topics for that given week.
-Thus, ensure your course notes are as detailed and complete as possible.
 
 Below, you will be given 3 critical pieces of information:
 - The student's goals, what they want to learn, and their background.
-- You will be given a specific week, which will be your focus for the generation of your course notes.
-- You will be given supplementary, relevant material, which you can utilize to help you generate the course notes.
+- The specific topic to focus your course notes on.
+- Additional supplementary material on that topip, which you can utilize to help you generate the course notes.
 
 Please output your final response in the list format below:
 ["Course Notes: ..."]
@@ -36,7 +29,7 @@ Please output your final response in the list format below:
 ##Student Goals/Background Information
 {student_info}
 
-##Target Week Information
+##Topic Information
 {week_information}
 
 ##Supplementary Material
@@ -45,28 +38,35 @@ Please output your final response in the list format below:
 ##Your Answer:
 """
 
-student_info = "The student wants to learn about symmetric cryptography and cryptographic hash functions. Their primary purpose is to understand how to programmatically encrypt and decrypt messages with different ciphers using Python. They are primarily interested in constructing simpler encryption and hash algorithms from scratch, aiming to comprehend these topics thoroughly by challenging themselves. While the student does not aim to apply these skills in a specific context right now, they are driven by a curiosity to understand these devices at a fundamental level. They don't currently plan to progress onto complicated aspects of cryptography, but this could potentially change as they delve deeper into the subject. Regarding their background, the student has a good grasp of Python programming and holds fundamental knowledge of mathematics and statistics, which would aid in comprehending cryptographic algorithms and hash functions. The student expects to dedicate 5 weeks to learning this material and will be working a few days a day."
-week_info = """Week 1: Understanding Symmetric Cryptography: 
-- Definition, purpose and application of Symmetric Cryptography
-- Studying popular Symmetric Key Algorithms like Data Encryption Standard (DES) and Advanced Encryption Standard (AES)
-- Understanding how encryption and decryption works in Symmetric Cryptography
-"""
+student_info_fp = '/Users/rahulduggal/Documents/personal_learnings/learning-assistant-v1/experimentation/course_gen/course_generation_round_one/student_background.txt'
+student_info_file = open(student_info_fp, 'r')
+student_info_text = student_info_file.read().strip()
 
-f_wiki = open('/Users/rahulduggal/Documents/personal_learnings/learning-assistant-v1/experimentation/part_three/wiki_symmetric_encryption.txt', 'r')
+# week_information_text = """Week 1: Fundamentals of Cryptography
+# - Definition and Basics of Cryptography
+# - Symmetric vs Asymmetric Key Cryptography
+# - Importance and real-life application of Cryptography
+# - Introduction to Python libraries for cryptography"""
+
+week_information_text = """Week 1: Fundamentals of Cryptography
+- Focused Purely On --> Definition and Basics of Cryptography"""
+
+f_wiki = open('/Users/rahulduggal/Documents/personal_learnings/learning-assistant-v1/experimentation/supplementary_material/wiki_symmetric_encryption.txt', 'r')
 wiki_lines = f_wiki.read().strip()
 
-f_tb = open('/Users/rahulduggal/Documents/personal_learnings/learning-assistant-v1/experimentation/part_three/applied_crypto_symmetric_encryption.txt', 'r')
+f_tb = open('/Users/rahulduggal/Documents/personal_learnings/learning-assistant-v1/experimentation/supplementary_material/applied_crypto_symmetric_encryption.txt', 'r')
 tb_lines = f_tb.read().strip()
 
-jy_tb = open('/Users/rahulduggal/Documents/personal_learnings/learning-assistant-v1/experimentation/part_three/joy_of_crypto_symmetric_encryption.txt', 'r')
-jy_lines = jy_tb.read().strip()
+# jy_tb = open('/Users/rahulduggal/Documents/personal_learnings/learning-assistant-v1/experimentation/supplementary_material/joy_of_crypto_symmetric_encryption.txt', 'r')
+# jy_lines = jy_tb.read().strip()
 
-supplementary_material = wiki_lines + '\n\n\n' + tb_lines + '\n\n\n' + jy_lines
+# supplementary_material = wiki_lines + '\n\n\n' + tb_lines + '\n\n\n' + jy_lines
+supplementary_material = wiki_lines + '\n\n\n' + tb_lines + '\n\n\n'
 print(f"Total length of supplementary material text: {len(supplementary_material)}")
 
 q_prompt = q_prompt.format(
-    student_info = student_info,
-    week_information = week_info,
+    student_info = student_info_text,
+    week_information = week_information_text,
     supplementary_material = supplementary_material,
 )
 print(f"Total length of prompt: {len(q_prompt)}")
@@ -80,5 +80,18 @@ chat_completion = client.chat.completions.create(
 )
 
 response_message = chat_completion.choices[0].message.content
-print(f"Response: {response_message}")
+# print(f"Response: {response_message}")
 
+output_dir_fp = '/Users/rahulduggal/Documents/personal_learnings/learning-assistant-v1/experimentation/course_gen/course_generation_round_one'
+# tmp_fp = os.path.join(output_dir_fp, f"week_one_full_course_notes.txt")
+tmp_fp = os.path.join(output_dir_fp, f"week_one_topic_one_course_notes.txt")
+f = open(tmp_fp, 'w')
+f.write(response_message)
+f.close()
+
+# TODO: 
+    # Keep experimenting here and main_four
+        # generation of the course notes + quiz/exercises <-- **need more relevant resources for course-note and quiz/exercise gen**
+        # once I have intuitive feel that this is good V1
+            # implement in app and literally use it to learn something
+            # go from there...

828480ce786876c2f0fc433c3ec3655a60e608bd
duggalr <duggalr42@gmail.com>
2024-04-16 19:59:28 -0400

    tests

--- tests/acc/test_views.py
+++ tests/acc/test_views.py
@@ -10,7 +10,7 @@ class LandingViewTest(TestCase):
     """
 
     def test_view_url_exists_at_desired_location(self):
-        response = self.client.get('')
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
     def test_view_accessible_by_name(self):
@@ -22,7 +22,7 @@ class LandingViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'generic/landing.html')
 
-    def test_view_creates_custom_user(self):
+    def test_view_has_custom_user(self):
         response = self.client.get('')
  
         self.assertTrue(CustomUser.objects.exists())
@@ -37,7 +37,7 @@ class LandingViewTest(TestCase):
 
         custom_user_obj = CustomUser.objects.first()
         self.assertIn('custom_user_uuid', self.client.session)
-        self.assertEqual(self.client.session['custom_user_uuid'], custom_user_obj.id)
+        self.assertEqual(self.client.session['custom_user_uuid'], str(custom_user_obj.id))
 
 
 class AboutViewTest(TestCase):
@@ -45,7 +45,42 @@ class AboutViewTest(TestCase):
     Testing core functions when user visits about page
     """
 
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/about')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_accessible_by_name(self):
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'generic/about.html')
+
+    def test_view_has_custom_user(self):
+        response = self.client.get('')
+ 
+        self.assertTrue(CustomUser.objects.exists())
+        self.assertTrue(AnonUser.objects.exists())
+
+        custom_user_obj = CustomUser.objects.first()
+        self.assertIsNotNone(custom_user_obj.anon_user)
+        self.assertIsNone(custom_user_obj.oauth_user)
+
+    def test_view_populates_session(self):
+        response = self.client.get('')
+
+        custom_user_obj = CustomUser.objects.first()
+        self.assertIn('custom_user_uuid', self.client.session)
+        self.assertEqual(self.client.session['custom_user_uuid'], str(custom_user_obj.id))
+
+
+class PlaygroundViewTest(TestCase):
+    """
+    """
     pass
 
+    
 
 

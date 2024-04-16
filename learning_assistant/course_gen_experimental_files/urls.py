f1155a068e578b48fc7934874af4068e09928d96
duggalr <duggalr42@gmail.com>
2024-04-16 09:41:45 -0400

    hiding experimental files

---
+++ learning_assistant/course_gen_experimental_files/urls.py
@@ -0,0 +1,9 @@
+
+# ## Course Gen
+    # path("course-gen/background-chat", views.course_generation_background_chat, name="course_gen_bg_chat"),
+    # path("course-gen/handle-background-message", views.handle_course_generation_background_message, name="handle_course_generation_background_message"),
+    # path("course-gen/course-outline/<int:cid>", views.student_course_outline, name="student_course_outline"),
+    # path("course-gen/course-home/<int:cid>", views.student_course_homepage, name="student_course_homepage"),
+    # path("course-gen/list", views.all_student_courses, name="all_student_courses"),
+    # path("course-gen/generate-module-notes", views.generate_module_notes, name="generate_module_notes"),
+    # path("course-gen/notes/<int:mid>", views.course_module_notes_view, name="course_module_notes_view"),
--- learning_assistant/models.py
+++ learning_assistant/models.py
@@ -1,14 +1,11 @@
 import uuid
 from django.db import models
-# from django.utils import timezone
 
 from acc.models import CustomUser
 
 
 class ChatConversation(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    # user_auth_obj = models.ForeignKey(UserOAuth, on_delete=models.CASCADE, blank=True, null=True)
-    # user_obj = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     question = models.CharField(max_length=3000)
     question_prompt = models.TextField()
     response = models.TextField()
@@ -32,117 +29,3 @@ class UserGeneralTutorParent(models.Model):
 
 class UserGeneralTutorConversation(ChatConversation):
     parent_obj = models.ForeignKey(UserGeneralTutorParent, on_delete=models.CASCADE, blank=True, null=True)
-
-    
-# class UserGeneralTutorConversation(models.Model):
-#     chat_parent_object = models.ForeignKey(UserGeneralTutorParent, on_delete=models.CASCADE, blank=True, null=True)
-#     question = models.CharField(max_length=3000)
-#     question_prompt = models.TextField()
-#     response = models.TextField()
-#     created_at = models.DateTimeField(auto_now_add=True)
-
-
-### Course Generation ###
-
-class CourseGenBackgroundParent(models.Model):
-    user_obj = models.ForeignKey(CustomUser, on_delete=models.CASCADE, blank=True, null=True)
-    final_response = models.TextField(blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-class CourseGenBackgroundConversation(ChatConversation):
-    bg_parent_obj = models.ForeignKey(CourseGenBackgroundParent, on_delete=models.CASCADE, blank=True, null=True)
-
-# class UserBackgroundParent(ChatParent):
-#     final_response = models.TextField(blank=True, null=True)
-
-class UserCourse(models.Model):
-    initial_background_object = models.ForeignKey(CourseGenBackgroundParent, on_delete=models.CASCADE, blank=True, null=True)
-    name = models.CharField(max_length=3000)
-    description = models.TextField()
-    module_list = models.TextField()
-    # outline = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-class UserCourseModules(models.Model):
-    parent_course_object = models.ForeignKey(UserCourse, on_delete=models.CASCADE, blank=True, null=True)
-    module_number = models.IntegerField(blank=True, null=True)
-    module_topic = models.TextField()
-    module_description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-class UserCourseModulesBingResult(models.Model):
-    parent_course_object = models.ForeignKey(UserCourse, on_delete=models.CASCADE, blank=True, null=True)
-    name = models.TextField()
-    url = models.TextField()
-    snippet = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-# TODO: 
-class UserCourseOutlineConversation(ChatConversation):
-    course_parent_object = models.ForeignKey(UserCourse, on_delete=models.CASCADE, blank=True, null=True)
-
-class UserCourseModuleNote(models.Model):
-    course_module_object = models.ForeignKey(UserCourseModules, on_delete=models.CASCADE, blank=True, null=True)
-    notes_md = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-class UserCourseModuleConversation(ChatConversation):
-    course_module_object = models.ForeignKey(UserCourseModules, on_delete=models.CASCADE, blank=True, null=True)
-
-class UserCourseModuleNoteQuestion(models.Model):
-    course_note_object = models.ForeignKey(UserCourseModuleNote, on_delete=models.CASCADE, blank=True, null=True)
-    question_number = models.IntegerField()
-    question = models.TextField()
-    multiple_choice_options = models.TextField(blank=True, null=True)
-    answer = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-
-
-
-
-
-# # class UserCourse(models.Model):
-# #     initial_background_object = models.ForeignKey(UserBackgroundParent, on_delete=models.CASCADE, blank=True, null=True)
-# #     name = models.CharField(max_length=3000)
-# #     description = models.TextField()
-# #     module_list = models.TextField()
-# #     # outline = models.TextField()
-# #     created_at = models.DateTimeField(auto_now_add=True)
-# #     updated_at = models.DateTimeField(auto_now=True)
-
-# # class UserCourseModules(models.Model):
-# #     parent_course_object = models.ForeignKey(UserCourse, on_delete=models.CASCADE, blank=True, null=True)
-# #     module_number = models.IntegerField(blank=True, null=True)
-# #     module_topic = models.TextField()
-# #     module_description = models.TextField()
-# #     created_at = models.DateTimeField(auto_now_add=True)
-
-# # class UserCourseModulesBingResult(models.Model):
-# #     parent_course_object = models.ForeignKey(UserCourse, on_delete=models.CASCADE, blank=True, null=True)
-# #     name = models.TextField()
-# #     url = models.TextField()
-# #     snippet = models.TextField()
-# #     created_at = models.DateTimeField(auto_now_add=True)
-
-# # class UserCourseOutlineConversation(ChatConversation):
-# #     course_parent_object = models.ForeignKey(UserCourse, on_delete=models.CASCADE, blank=True, null=True)
-
-# # class UserCourseModuleNote(models.Model):
-# #     course_module_object = models.ForeignKey(UserCourseModules, on_delete=models.CASCADE, blank=True, null=True)
-# #     notes_md = models.TextField()
-# #     created_at = models.DateTimeField(auto_now_add=True)
-
-# # class UserCourseModuleConversation(ChatConversation):
-# #     course_module_object = models.ForeignKey(UserCourseModules, on_delete=models.CASCADE, blank=True, null=True)
-
-# # class UserCourseModuleNoteQuestion(models.Model):
-# #     course_note_object = models.ForeignKey(UserCourseModuleNote, on_delete=models.CASCADE, blank=True, null=True)
-# #     question_number = models.IntegerField()
-# #     question = models.TextField()
-# #     multiple_choice_options = models.TextField(blank=True, null=True)
-# #     answer = models.TextField()
-# #     created_at = models.DateTimeField(auto_now_add=True)
-
